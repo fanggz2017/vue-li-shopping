0d42fffdcf9b9074d3ac8b21f6f2f9d5e2e9c84f
方国桢 <fangguozhen@live.com>
2018-05-19 21:42:58 +0800

    postcss-vw

--- .postcssrc.js
+++ .postcssrc.js
@@ -4,8 +4,12 @@ module.exports = {
   "plugins": {
     "postcss-import": {},
     "postcss-url": {},
+    "postcss-aspect-ratio-mini": {},
+    "postcss-write-svg": {
+       utf8: false
+    },
+    "postcss-cssnext": {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {},
     "postcss-px-to-viewport": {
        viewportWidth: 750,
        viewportHeight: 1344,
@@ -14,6 +18,12 @@ module.exports = {
        selectorBlackList: [],
        minpixelValue: 1,
        mediaQuery: false  
+    },
+    "postcss-viewport-units": {},
+    "cssnano": {
+      preset: "advanced",
+      autoprefixer: false,
+      "postcss-zindex": false
     }
   }
 }
