fb2cb17a7339c3be6f1c07a22e450b489b7983ca
方国桢 <fangguozhen93@gmail.com>
2018-05-20 17:06:50 +0800

    login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,13 +10,12 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+     
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="postcss" scoped>
 #login-input{
   & div{
