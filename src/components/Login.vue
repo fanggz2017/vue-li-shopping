9f771bc42b6e76203e13a5e31bab5fe974394237
fangguozhen <fangguozhen93@gmail.com>
2018-05-21 16:06:18 +0800

    login & register

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,23 +1,59 @@
 <template>
 <div id="login-input">
-    <div><label>账号</label><input type="tel" maxlength="11" placeholder="请输入手机号"></div>
-    <div><label>密码</label><input type="passWord" placeholder="请输入密码"></div>
+    <h4>登录</h4>
+    <div><label>账号</label><input type="tel" maxlength="11" placeholder="请输入手机号" v-model="account.phone"></div>
+    <div><label>密码</label><input type="passWord" placeholder="请输入密码" v-model="account.passWord"></div>
+    <a class="submit" :class="{disabled:disabled}" @click="submit"><span>登录</span></a>
+    <p>
+      <router-link to="/home/login">手机快速注册</router-link>
+      <router-link to="/home/fogetPassword">找回密码</router-link>
+    </p>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Login',
   data () {
     return {
-     
+      account: {
+        phone: '',
+        passWord: ''
+      },
+      disabled: true
+    }
+  },
+  methods: {
+    submit () {
+      if (!this.disabled) {
+        alert('登录方法暂时还没写')
+      }  
+    }
+  },
+   watch: {
+    account: {
+      handler: function (value) {
+        if (this.account.phone !== '' && this.account.passWord !== '') {
+          this.disabled = false
+        } else {
+          this.disabled = true
+        }
+      },
+      deep: true
     }
   }
 }
 </script>
 
+<style lang="postcss" src="../assets/style/common.css"> </style>
 <style lang="postcss" scoped>
 #login-input{
+  & h4{
+    font-weight: 100;
+    letter-spacing: 3px;
+    font-size: 38px;
+    margin-bottom: 100px;
+  }
   & div{
     margin: 0 auto;
     position: relative;
@@ -25,19 +61,20 @@ export default {
     width: 640px;
     height: 80px;
     line-height: 80px;
-    font-size: 24px;
+    font-size: 30px;
     & input{
       width: 500px;
-      height: 40px;
-      line-height: 40px;
+      height: 45px;
+      line-height: 45px;
       border: none;
       padding-left: 30px;
-      color: #333;  
+      font-size: 30px;
+      color: #333;
       &:focus{
         outline: none;
       }
       &::-webkit-input-placeholder{
-        font-size: 24px;
+        font-size: 30px;
       }
     }
     &:after{
@@ -49,6 +86,33 @@ export default {
       height: 1px;
       width: 640px;
     }
-  } 
+  }
+  & .submit{
+    display: block;
+    margin: 0 auto;
+    width: 640px;
+    height: 80px; 
+    line-height: 80px;
+    background: #f00;
+    border-radius: 8px;
+    color: #fff;
+    font-size: 30px;
+    margin-top: 60px;
+    letter-spacing: 3px;
+  }
+  & .submit.disabled span{
+    opacity: 0.5;
+  }
+  & p{
+    display: flex;
+    justify-content: space-between;
+    width: 640px;
+    margin: 0 auto;
+    margin-top: 60px;
+    & a{
+      color: #666;
+      font-size: 26px;
+    }
+  }
 }
 </style>
