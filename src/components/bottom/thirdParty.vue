379344d1591c40eb80b237841ae80a58f6d0cf41
fangguozhen <fangguozhen93@gmail.com>
2018-05-22 17:51:01 +0800

    third-login

--- src/components/bottom/thirdParty.vue
+++ src/components/bottom/thirdParty.vue
@@ -1,11 +1,17 @@
 <template>
-<div>
+<div id="third-login">
+  <div class="third-login-wechat">
     <svg class="icon" aria-hidden="true">
       <use xlink:href="#icon-social-wechat"></use>
-    </svg>  
+    </svg> 
+    <span>微信登录</span> 
+  </div>
+  <div class="third-login-qq">
      <svg class="icon" aria-hidden="true">
       <use xlink:href="#icon-social-qq"></use>
     </svg> 
+    <span>QQ登录</span> 
+  </div>
 </div>
 </template>
 
@@ -18,11 +24,21 @@ export default {
 </script>
 
 <style <style lang="postcss" scoped>
-  div{
-    position: fixed;
-    bottom: 100px;
+  #third-login{
+    display: flex;
+    width: 60vw;
+    margin: 0 auto;
+    justify-content: space-between;
+    margin-top: calc(100vh - 120vw);
+    margin-left: 20vw;
+    & .third-login-wechat span,& .third-login-qq span{
+      display: block;
+      font-size: 24px;
+      margin-top: 10px;
+    }
     & .icon {
-       width: 1em; height: 1em;
+       width: 60px;
+       height: 60px;
        vertical-align: -0.15em;
        fill: currentColor;
        overflow: hidden;
